fda54ea11c678e214b0fe8d2f1a19809fa4f8a0b
Christopher Moore <chmoore889@gmail.com>
2020-01-30 00:12:10 -0500

    Added multithreading
    Added multithreading for driving controls

--- src/main/java/frc/robot/Driver.java
+++ src/main/java/frc/robot/Driver.java
@@ -10,13 +10,15 @@ public class Driver {
     Controller joy;
     Drivetrain drive;
 
-    private SendableChooser<Byte> driveType;
+    SendableChooser<Byte> driveType;
     private final Byte arcade = 0;
     private final Byte tank = 1;
     
-    private SendableChooser<Byte> neutralModeType;//Sets whether motors brake or coast
+    SendableChooser<Byte> neutralModeType;//Sets whether motors brake or coast
     private final Byte brake = 0;
     private final Byte coast = 1;
+    
+    DriveThread driveThread;
 
     public Driver(int port) {
         joy = new Controller(port);
@@ -34,30 +36,59 @@ public class Driver {
         neutralModeType.addOption("Coast", coast);
         SmartDashboard.putData("Drivetrain Coast Type", neutralModeType);
     }
-
-    public void runDriveControls() {
-        //driveType Selection
-        if(driveType.getSelected().equals(arcade)) {
-            drive.arcadeDrive(joy.getRightYAxis(), joy.getLeftTrigger(), joy.getRightTrigger());
-            //System.out.println("Y Axis: " + joy.getRightYAxis() + "     Left: " + joy.getLeftTrigger() + "     Right: " + joy.getRightYAxis());
-        } 
-        else if(driveType.getSelected().equals(tank)) {
-            drive.tankDrive(joy.getLeftYAxis(), joy.getRightYAxis());
-        } 
-        else {
-            System.out.println("Error: No drive type chosen");
-        }
-        
-        //neutralModeType Selection
-        if(neutralModeType.getSelected().equals(brake)) {
-            drive.setNeutralMode(NeutralMode.Brake);
-        }
-        else if(neutralModeType.getSelected().equals(coast)) {
-            drive.setNeutralMode(NeutralMode.Coast);
-        }
-        else {
-            System.out.println("Error: No coast type chosen");
-        }
+    
+    public void initDriveThread() {
+        //Thread initialization
+        driveThread = new DriveThread(this);
+		new Thread(driveThread).start();
     }
+	
+	private class DriveThread implements Runnable {
+		Drivetrain drive;
+		SendableChooser<Byte> driveType;
+		SendableChooser<Byte> neutralModeType;
 
+		public PlotThread(Driver driver) {
+			this.drive = driver.drive;
+			this.driveType = driver.driveType;
+			this.neutralModeType = driver.neutralModeType;
+		}
+
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(20);//Sleep for 20ms to reduse used network bandwidth
+				} 
+                catch (Exception e) {
+				}
+
+				runDriveControls();
+			}
+		}
+        
+        private void runDriveControls() {
+            //driveType Selection
+            if(driveType.getSelected().equals(arcade)) {
+                drive.arcadeDrive(joy.getRightYAxis(), joy.getLeftTrigger(), joy.getRightTrigger());
+                //System.out.println("Y Axis: " + joy.getRightYAxis() + "     Left: " + joy.getLeftTrigger() + "     Right: " + joy.getRightYAxis());
+            } 
+            else if(driveType.getSelected().equals(tank)) {
+                drive.tankDrive(joy.getLeftYAxis(), joy.getRightYAxis());
+            } 
+            else {
+                System.out.println("Error: No drive type chosen");
+            }
+
+            //neutralModeType Selection
+            if(neutralModeType.getSelected().equals(brake)) {
+                drive.setNeutralMode(NeutralMode.Brake);
+            }
+            else if(neutralModeType.getSelected().equals(coast)) {
+                drive.setNeutralMode(NeutralMode.Coast);
+            }
+            else {
+                System.out.println("Error: No coast type chosen");
+            }
+        }
+	}
 }
